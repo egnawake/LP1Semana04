7c9bf7a89f05b2cf87948d359fbd2b54a97d8a82
João Inácio <egnawake@gmail.com>
2022-03-16 21:25:32 +0000

    Add FamousQuotes project

---
+++ FamousQuotes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace FamousQuotes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("You talking to me?");
+            MyPrecious();
+            Console.WriteLine("Dodge this");
+            Hasta();
+
+            // You talking to me?
+            // Show me the money!
+            // My precious.
+            // Dodge this
+            // Show me the money!
+            // Hasta la vista, baby.
+            // Show me the money!
+            // My precious.
+        }
+
+        private static void Hasta()
+        {
+            TheMoney();
+            Console.WriteLine("Hasta la vista, baby.");
+            MyPrecious();
+        }
+
+        private static void TheMoney()
+        {
+            Console.WriteLine("Show me the money!");
+        }
+
+        private static void MyPrecious()
+        {
+            TheMoney();
+            Console.WriteLine("My precious.");
+        }
+    }
+}
