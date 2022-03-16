49f171a047fe59ee26d2b0a668095eec88914252
João Inácio <egnawake@gmail.com>
2022-03-16 22:07:32 +0000

    MyFirstMethod: document methods

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -4,6 +4,10 @@ namespace MyFirstMethod
 {
     class Program
     {
+        /// <summary>
+        /// Program entry point.
+        /// </summary>
+        /// <param name="args">Command line arguments.</param>
         static void Main(string[] args)
         {
             CountToN(7);
@@ -11,11 +15,18 @@ namespace MyFirstMethod
             CountToN(13);
         }
 
+        /// <summary>
+        /// Prints integers from 1 to 10.
+        /// </summary>
         private static void CountTo10()
         {
             CountToN(10);
         }
 
+        /// <summary>
+        /// Prints integers from 1 to n.
+        /// </summary>
+        /// <param name="n">Last integer to be printed.</param>
         private static void CountToN(int n)
         {
             for (int i = 1; i <= n; i++)
