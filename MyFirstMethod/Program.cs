7444430feaf0dd16a6a868397fcb8e45c82a51f9
João Inácio <egnawake@gmail.com>
2022-03-16 21:12:27 +0000

    Call CountTo10 from Main

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -6,7 +6,8 @@ namespace MyFirstMethod
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CountTo10();
+            CountTo10();
         }
 
         private static void CountTo10()
