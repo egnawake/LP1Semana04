e0b5f5fcfb61092a05d845acf70c86227f2cf27d
João Inácio <egnawake@gmail.com>
2022-03-16 21:40:11 +0000

    MyFirstMethod: Add CountToN method

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -6,13 +6,22 @@ namespace MyFirstMethod
     {
         static void Main(string[] args)
         {
-            CountTo10();
-            CountTo10();
+            CountToN(7);
+            Console.WriteLine("");
+            CountToN(13);
         }
 
         private static void CountTo10()
         {
             for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        private static void CountToN(int n)
+        {
+            for (int i = 1; i <= n; i++)
             {
                 Console.WriteLine(i);
             }
