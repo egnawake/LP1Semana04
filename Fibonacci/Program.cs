0cd4dea1eab8f9fa13a12eedda9528187a6bedfc
João Inácio <egnawake@gmail.com>
2022-03-16 22:54:27 +0000

    Fibonacci: add method to get Fibonacci sequence terms

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -6,7 +6,23 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // ask user for term
+            Console.WriteLine("Enter a term: ");
+            int term = int.Parse(Console.ReadLine());
+
+            // print result
+            int result = Fibonacci(term);
+            Console.WriteLine($"Term {term}: {result}");
+        }
+
+        private static int Fibonacci(int n)
+        {
+            if (n == 1 || n == 2)
+            {
+                return 1;
+            }
+
+            return Fibonacci(n - 2) + Fibonacci(n - 1);
         }
     }
 }
