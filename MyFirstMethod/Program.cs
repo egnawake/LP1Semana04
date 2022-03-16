776cc18e94dc93ab4344c2884240ca9bdb6dd2a7
João Inácio <egnawake@gmail.com>
2022-03-16 21:11:53 +0000

    Add CountTo10 method

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -8,5 +8,13 @@ namespace MyFirstMethod
         {
             Console.WriteLine("Hello World!");
         }
+
+        private static void CountTo10()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
     }
 }
