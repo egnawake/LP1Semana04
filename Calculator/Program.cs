d2b70e84d4489b4124f55551276dd932f55d2ab9
João Inácio <egnawake@gmail.com>
2022-03-17 15:49:26 +0000

    bases_cs/23: Implement Calculator

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,7 +6,72 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double a, b, result;
+            char op;
+
+            // validate number of arguments
+            if (args.Length != 3)
+            {
+                Console.WriteLine($"Expected 3 arguments, got {args.Length}");
+                return;
+            }
+
+            // parse arguments
+            op = char.Parse(args[0]);
+            a = double.Parse(args[1]);
+            b = double.Parse(args[2]);
+
+            // get result based on selected operation
+            // if operation is invalid, show error and return
+            switch (op)
+            {
+                case '+':
+                    result = Add(a, b);
+                    break;
+                case '-':
+                    result = Subtract(a, b);
+                    break;
+                case '/':
+                    result = Divide(a, b);
+                    break;
+                case 'x':
+                    result = Multiply(a, b);
+                    break;
+                case 'p':
+                    result = Power(a, b);
+                    break;
+                default:
+                    Console.WriteLine("Unknown operation");
+                    return;
+            }
+
+            // print result
+            Console.WriteLine(result);
+        }
+
+        private static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        private static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        private static double Divide(double a, double b)
+        {
+            return a / b;
+        }
+
+        private static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        private static double Power(double a, double b)
+        {
+            return Math.Pow(a, b);
         }
     }
 }
