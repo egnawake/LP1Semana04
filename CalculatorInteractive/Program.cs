7fb6973b2b1775ba7133736c7f7faf1b0341a5d4
João Inácio <egnawake@gmail.com>
2022-03-17 15:50:27 +0000

    bases_cs/24: Create project

---
+++ CalculatorInteractive/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculatorInteractive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
