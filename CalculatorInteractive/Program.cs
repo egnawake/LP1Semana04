0bc97d737ecc7723d7b2e5b93e48d5247bb3be82
João Inácio <egnawake@gmail.com>
2022-03-22 21:50:33 +0000

    CalcInteractive: copy Main from Calculator

--- CalculatorInteractive/Program.cs
+++ CalculatorInteractive/Program.cs
@@ -6,7 +6,73 @@ namespace CalculatorInteractive
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double a, b, result;
+            string op;
+
+            while (op != "END")
+            {
+                // ask user for arguments
+                Console.Write("Op: ");
+                op = Console.ReadLine();
+                Console.Write("First number: ");
+                a = double.Parse(Console.ReadLine());
+                Console.Write("Second number: ");
+                b = double.Parse(Console.ReadLine());
+
+                // get result based on selected operation
+                // if operation is invalid, show error and return
+                switch (op)
+                {
+                    case "+":
+                        result = Add(a, b);
+                        break;
+                    case "-":
+                        result = Subtract(a, b);
+                        break;
+                    case "/":
+                        result = Divide(a, b);
+                        break;
+                    case "x":
+                        result = Multiply(a, b);
+                        break;
+                    case "p":
+                        result = Power(a, b);
+                        break;
+                    case "END":
+                        break;
+                    default:
+                        Console.WriteLine("Unknown operation");
+                        break;
+                }
+
+                // print result
+                Console.WriteLine(result);
+            }
+        }
+
+        private static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        private static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        private static double Divide(double a, double b)
+        {
+            return a / b;
+        }
+
+        private static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        private static double Power(double a, double b)
+        {
+            return Math.Pow(a, b);
         }
     }
 }
